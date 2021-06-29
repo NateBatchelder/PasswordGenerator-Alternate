09d7ea59690333bdeb90d77d13bf97947dc3fb72
Nathan Batchelder <natebatchelder@gmail.com>
2021-06-29 00:31:43 -0700

    final save, some previous edits had not been saved

--- style.js
+++ style.js
@@ -72,7 +72,6 @@ function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) +97);
 }
 
-
 function getRandomUpper() {
     return String.fromCharCode(Math.floor(Math.random() * 26) +65);
 }
