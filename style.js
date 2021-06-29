a75b4e422be0ac8cfd1aee467a92318d5323599a
Nathan Batchelder <natebatchelder@gmail.com>
2021-06-28 23:48:13 -0700

    functions for random values edit

--- style.js
+++ style.js
@@ -1,8 +1,18 @@
 // user options
 
 function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) +97));
+    return String.fromCharCode(Math.floor(Math.random() * 26) +97);
 }
 
-console.log(getRandomLower());
 
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) +65);
+}
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() *10) +48);
+}
+
+function getRandomSpecial() {
+
+}
